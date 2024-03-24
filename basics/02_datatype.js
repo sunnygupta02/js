35472774758e1c15a0c64cbb556673d7a03fd23b
sunnygupta02 <119126667+sunnygupta02@users.noreply.github.com>
2024-03-24 18:06:04 +0000

    datatype

--- basics/02_datatype.js
+++ basics/02_datatype.js
@@ -1,1 +1,26 @@
 "use strict"; //treat js code as newer version
+//alert(3+3) we are using node js not browser
+let name="sunnny";
+let age=21;
+let login =true;
+let state;
+
+console.log(typeof login);
+console.log(typeof null); //-> object interview question
+console.log(typeof state);
+
+/*
+datatype:
+primitive:
+number 
+bigInt 
+string 
+null -> standalone value, object
+undefined
+boolean
+symbol ->unique
+
+non-primitive:
+object
+
+*/
--- basics/variable.js
+++ basics/variable.js
@@ -4,7 +4,8 @@ var accountPassword="12345"
 accountCity="delhi"
 console.log(accountCity);
 let accountState;
-
+accountEmail="sunny@gmail.com";
+accountPassword="234";
 /*
 prefer not to use var because of issue of block scope and functional scope
 */
